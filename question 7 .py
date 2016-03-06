d4c805e29073de1963933fbfb8a6dc34be1112d5
famillefg Giraud <famillefg Giraud>
2016-03-06 18:27:47 +0100

    menage question7

--- question 7 .py
+++ question 7 .py
@@ -1,15 +1,14 @@
-zero_par_secante(O0,O1,theta_moteur,epsilon,f1,ecart,e):
-
-
-
 from math import *
-liste_valeurs_e=[150,160,170,180,190,200]
+from projet111 import *
+
+
+liste_valeurs_e = [150, 160, 170, 180, 190, 200]
 for i in range(len(liste_valeurs_e)):
-    e=liste_valeurs_e[i]
-    L=[]
+    e = liste_valeurs_e[i]
+    L = []
     for k in range(360):
-        O1=k*2*pi/360
-        L.append(zero_par_secante(O0,O1,theta_moteur,epsilon,f1,ecart,e))
-    max,min=max(L),min(L)
-    debattement=max-min
-    print("le debattement angulaire de e=",liste_valeurs_e[i],"est:",debattement)
+        O1 = k*2*pi/360
+        L.append(zero_par_secante(O0, O1, theta_moteur, epsilon, f1, ecart, e))
+    max, min = max(L), min(L)
+    debattement = max - min
+    print("le debattement angulaire de e=", liste_valeurs_e[i], "est:", debattement)
